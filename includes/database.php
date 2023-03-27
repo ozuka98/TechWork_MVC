1b2db36a92d8608d0f8caf8c38f90a11f0ac139d
Oscar <cabreraoscar601@gmail.com>
2023-03-26 22:31:40 -0600

    Modificacion htaccess y database

--- includes/database.php
+++ includes/database.php
@@ -1,9 +1,9 @@
 <?php
 $db = mysqli_connect(
-    $_ENV['DB_HOST'] ?? 'us-cdbr-east-06.cleardb.net',
-    $_ENV['DB_USER'] ?? 'b824cc74451e96', 
-    $_ENV['DB_PASS'] ?? '7e94e75d', 
-    $_ENV['DB_NAME'] ?? 'heroku_8f7b1849124d968'
+    $_ENV['DB_HOST'] ?? '',
+    $_ENV['DB_USER'] ?? '', 
+    $_ENV['DB_PASS'] ?? '', 
+    $_ENV['DB_NAME'] ?? ''
 );
 
 if (!$db) {
